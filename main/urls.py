9d9679f748fe0bfd7c7d1acd47d9ce0a23836543
JMoran1 <45739562+JMoran1@users.noreply.github.com>
2023-08-08 21:22:05 +0100

    Added user login

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from main import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
+from .forms import LoginForm
+
+
+@login_required
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, 'main/home.html')
+
+
+class CustomLoginView(LoginView):
+    template_name = 'main/login.html'
+    authentication_form = LoginForm
+    redirect_authenticated_user = True
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
