f5897d176ed68475ac582120a02a168649fd5162
JMoran1 <45739562+JMoran1@users.noreply.github.com>
2023-08-08 21:32:27 +0100

    Added new tests

--- main/tests.py
+++ main/tests.py
@@ -1,10 +1,49 @@
 from django.test import TestCase
 from django.urls import reverse
 
-# Create your tests here.
-# Write a test to make sure that the home page returns a 200 HTTP status code.
+from app_auth.models import User
+
+def create_test_user_of_user_type(user_type: User.user_type):
+    user = User.objects.create(username='Test', user_type=user_type)
+    user.set_password('Test')
+    user.save()
+    return user
+
 class HomeTests(TestCase):
     def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
+
+class StandardPageLoginTests(TestCase):
+    def setUp(self):
+        self.user = create_test_user_of_user_type(User.UserType.STANDARD)
+        self.client.force_login(self.user)
+
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
+
+class AdminPageLoginTests(TestCase):
+    def setUp(self):
+        self.user = create_test_user_of_user_type(User.UserType.ADMIN)
+        self.client.force_login(self.user)
+
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+
+class LoginTests(TestCase):
+    def test_login_view_status_code(self):
+        url = reverse('login')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+class LogoutTests(TestCase):
+    def test_logout_view_status_code(self):
+        url = reverse('logout')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
